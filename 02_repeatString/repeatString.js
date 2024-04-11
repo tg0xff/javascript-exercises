58c5e20774b0965ca5953326a7fde6a5b235fedc
Selene Tamez Galván <tg.sln255@gmail.com>
2024-04-11 16:04:46 -0600

    Solve 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function(string, repetitions) {
+  if (repetitions < 0) {
+    return "ERROR";
+  }
+
   let newString = "";
   for (let i = 0; i < repetitions; i++) {
     newString += string;
