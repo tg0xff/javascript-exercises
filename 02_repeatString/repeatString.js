a0e577829d6e832cffcef8ac31209f8c7759a487
tg0xff <tg0xff@proton.me>
2024-04-11 16:03:06 -0600

    Pass 2nd exercise's tests 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+  let newString = "";
+  for (let i = 0; i < repetitions; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
 // Do not edit below this line
