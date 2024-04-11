d8f975f9cbebd34964f3d7ba870d8b071748e6b7
tg0xff <tg0xff@proton.me>
2024-04-11 17:14:18 -0600

    Implement convertToCelsius() from exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,8 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  celsius = celsius.toFixed(1)
+  celsius = +celsius;
+  return celsius;
 };
 
 const convertToFahrenheit = function() {
