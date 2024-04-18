1e090c0b96c58fa3c7a986917ab38241dadc571a
tg0xff <tg0xff@proton.me>
2024-04-18 14:10:54 -0600

    Pass first 5 tests of ex. 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthNumber) {
+  let secuenceHolder = [1, 1];
+  let indexToggle = 0;
+  for (let i = 3; i <= nthNumber; i++) {
+    indexToggle = (indexToggle === 0) ? 1 : 0;
+    secuenceHolder[indexToggle] = secuenceHolder[0] + secuenceHolder[1];
+  }
+  return secuenceHolder[indexToggle];
 };
 
 // Do not edit below this line
