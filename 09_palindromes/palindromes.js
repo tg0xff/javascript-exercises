a7c5d6f5dbe20623ea688c6074e403fa23a0ef54
tg0xff <tg0xff@proton.me>
2024-04-18 13:51:54 -0600

    Pass 5 tests of ex. 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  let cleanString = s.split("");
+  cleanString = cleanString.filter((char) => /\w/.test(char));
+  let reverseString = cleanString.reverse();
+  cleanString = cleanString.join("");
+  reverseString = reverseString.join("");
+  return (cleanString === reverseString) ? true : false;
 };
 
 // Do not edit below this line
