f10aa8e66c32cb5f467ad1e18f4106ca56560741
tg0xff <tg0xff@proton.me>
2024-04-18 14:39:11 -0600

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,17 @@
 const findTheOldest = function (people) {
   return people.reduce(
     (oldest, person) => {
-      const personAge = person.yearOfDeath - person.yearOfBirth;
-      const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+      const currentYear = new Date().getFullYear();
+      let personKnownYear = person.yearOfDeath;
+      let oldestKnownYear = oldest.yearOfDeath;
+      if (personKnownYear === undefined) {
+        personKnownYear = currentYear;
+      }
+      if (oldestKnownYear === undefined) {
+        oldestKnownYear = currentYear;
+      }
+      const personAge = personKnownYear - person.yearOfBirth;
+      const oldestAge = oldestKnownYear - oldest.yearOfBirth;
       return (personAge > oldestAge) ? person : oldest;
     },
     { yearOfDeath: 0, yearOfBirth: 0 },
