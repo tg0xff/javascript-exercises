03daff509ea73cfc0baa8b490edfb89b7cd14c1d
tg0xff <tg0xff@proton.me>
2024-04-18 14:27:11 -0600

    Pass first two tests of ex. 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce(
+    (oldest, person) => {
+      const personAge = person.yearOfDeath - person.yearOfBirth;
+      const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+      return (personAge > oldestAge) ? person : oldest;
+    },
+    { yearOfDeath: 0, yearOfBirth: 0 },
+  );
 };
 
 // Do not edit below this line
