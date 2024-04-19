ef72f4dbc4363a27d6765192c33ab37437392473
Selene Tamez Galván <tg.sln255@gmail.com>
2024-04-18 20:36:02 -0600

    Fix typo in ex. 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,13 +3,13 @@ const fibonacci = function(nthNumber) {
   if (nthNumber === 0) return 0;
   if (nthNumber < 0) return "OOPS";
 
-  let secuenceHolder = [1, 1];
+  let sequenceHolder = [1, 1];
   let indexToggle = 0;
   for (let i = 3; i <= nthNumber; i++) {
     indexToggle = (indexToggle === 0) ? 1 : 0;
-    secuenceHolder[indexToggle] = secuenceHolder[0] + secuenceHolder[1];
+    sequenceHolder[indexToggle] = sequenceHolder[0] + sequenceHolder[1];
   }
-  return secuenceHolder[indexToggle];
+  return sequenceHolder[indexToggle];
 };
 
 // Do not edit below this line
